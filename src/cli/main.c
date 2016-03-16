f47a6d53c04984faf895fc9b2085d043cb9baea8
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-03-16 12:48:53 +0100

    cli: remove global variable
    Use an extra value on the stack to decide if we want to print the
    output.

--- src/cli/main.c
+++ src/cli/main.c
@@ -18,8 +18,6 @@
 	"delete dukCommit;"
 
 
-static int interactive_mode = 0;
-
 static int get_stack_raw(duk_context *ctx) {
 	if (!duk_is_object(ctx, -1)) {
 		return 1;
@@ -52,46 +50,23 @@ static void print_pop_error(duk_context *ctx, FILE *f) {
 static int wrapped_compile_execute(duk_context *ctx) {
 	const char *src_data;
 	duk_size_t src_len;
+	duk_bool_t write_output;
 
-	/* [ ... src_data src_len filename ] */
+	/* [ ... write_output src_data src_len filename ] */
 
+	write_output = duk_require_boolean(ctx, -4);
 	src_data = (const char *) duk_require_pointer(ctx, -3);
 	src_len = (duk_size_t) duk_require_uint(ctx, -2);
 	duk_compile_lstring_filename(ctx, 0, src_data, src_len);
 
-	/* [ ... src_data src_len function ] */
+	/* [ ... write_output src_data src_len function ] */
 
 	duk_push_global_object(ctx);  /* 'this' binding */
 	duk_call_method(ctx, 0);
 
-	if (interactive_mode) {
-		/*
-		 *  In interactive mode, write to stdout so output won't
-		 *  interleave as easily.
-		 *
-		 *  NOTE: the ToString() coercion may fail in some cases;
-		 *  for instance, if you evaluate:
-		 *
-		 *    ( {valueOf: function() {return {}},
-		 *       toString: function() {return {}}});
-		 *
-		 *  The error is:
-		 *
-		 *    TypeError: failed to coerce with [[DefaultValue]]
-		 *            duk_api.c:1420
-		 *
-		 *  These are handled now by the caller which also has stack
-		 *  trace printing support.  User code can print out errors
-		 *  safely using duk_safe_to_string().
-		 */
-
+	if (write_output) {
 		fprintf(stdout, "= %s\n", duk_to_string(ctx, -1));
 		fflush(stdout);
-	} else {
-		/* In non-interactive mode, success results are not written at all.
-		 * It is important that the result value is not string coerced,
-		 * as the string coercion may cause an error in some cases.
-		 */
 	}
 
 	return 0;  /* duk_safe_call() cleans up */
@@ -135,13 +110,12 @@ static int handle_stdin(duk_context *ctx) {
 		bufoff += got;
 	}
 
+	duk_push_false(ctx);
 	duk_push_pointer(ctx, (void *) buf);
 	duk_push_uint(ctx, (duk_uint_t) bufoff);
 	duk_push_string(ctx, "stdin");
 
-	interactive_mode = 0;  /* global */
-
-	rc = duk_safe_call(ctx, wrapped_compile_execute, 3 /*nargs*/, 1 /*nret*/);
+	rc = duk_safe_call(ctx, wrapped_compile_execute, 4 /*nargs*/, 1 /*nret*/);
 
 	free(buf);
 	buf = NULL;
@@ -172,13 +146,12 @@ static int handle_eval(duk_context *ctx, char *code) {
 	int rc;
 	int retval = -1;
 
+	duk_push_false(ctx);
 	duk_push_pointer(ctx, (void *) code);
 	duk_push_uint(ctx, (duk_uint_t) strlen(code));
 	duk_push_string(ctx, "eval");
 
-	interactive_mode = 0;  /* global */
-
-	rc = duk_safe_call(ctx, wrapped_compile_execute, 3 /*nargs*/, 1 /*nret*/);
+	rc = duk_safe_call(ctx, wrapped_compile_execute, 4 /*nargs*/, 1 /*nret*/);
 
 	if (rc != DUK_EXEC_SUCCESS) {
 		print_pop_error(ctx, stderr);
@@ -205,13 +178,12 @@ static int handle_interactive(duk_context *ctx) {
 		    linenoiseHistoryAdd(line);
 		}
 
+		duk_push_true(ctx);
 		duk_push_pointer(ctx, (void *) line);
 		duk_push_uint(ctx, (duk_uint_t) strlen(line));
 		duk_push_string(ctx, "input");
 
-		interactive_mode = 1;  /* global */
-
-		rc = duk_safe_call(ctx, wrapped_compile_execute, 3 /*nargs*/, 1 /*nret*/);
+		rc = duk_safe_call(ctx, wrapped_compile_execute, 4 /*nargs*/, 1 /*nret*/);
 
 		free(line);
 
