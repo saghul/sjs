83ded675a48c587af8d1aea8aabbb1845746634a
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-03-17 01:22:01 +0100

    vm: set custom fatal error handler in the duktape heap

--- src/vm/vm.c
+++ src/vm/vm.c
@@ -1,9 +1,17 @@
 
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "duktape.h"
 #include "vm.h"
+
+
+static void sjs__duk_fatal_handler(duk_context *ctx, duk_errcode_t code, const char *msg) {
+    fprintf(stderr, "FATAL %ld: %s\n", (long) code, (const char *) (msg ? msg : "null"));
+    fflush(stderr);
+    abort();
+}
 
 
 DUK_EXTERNAL sjs_vm_t* sjs_vm_create(void) {
@@ -11,11 +19,11 @@ DUK_EXTERNAL sjs_vm_t* sjs_vm_create(void) {
     vm = calloc(1, sizeof(*vm));
     assert(vm != NULL);
 
-    vm->ctx = duk_create_heap(NULL,          /* alloc function */
-			      NULL,          /* realloc function */
-			      NULL,          /* free function */
-			      (void*) vm,    /* user data */
-			      NULL           /* fatal error handler */
+    vm->ctx = duk_create_heap(NULL,                     /* alloc function */
+			      NULL,                     /* realloc function */
+			      NULL,                     /* free function */
+			      (void*) vm,               /* user data */
+			      sjs__duk_fatal_handler    /* fatal error handler */
 			     );
     assert(vm->ctx != NULL);
     return vm;
