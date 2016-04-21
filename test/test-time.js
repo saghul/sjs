515dd0951e4f3f41c3cfe6ab1c10f6373a659d1a
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-04-21 19:13:44 +0200

    test: add a test for the time module

---
+++ test/test-time.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const assert = require('assert');
+const time = require('time');
+
+
+var t = time.time();
+assert.ok(t > 0);
+
+var t1 = time.hrtime();
+time.sleep(1.5);
+var diff = time.hrtime(t1);
+assert.ok(diff[0] >= 1);
+
