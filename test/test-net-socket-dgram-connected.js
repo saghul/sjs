37044c1fcdbb154bf41fe894aed06f6849406058
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-04-28 09:42:42 +0200

    test: add a couple of dgram socket tests

---
+++ test/test-net-socket-dgram-connected.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const assert = require('assert');
+const net = require('net');
+
+
+const TEST_ADDR = {host: '127.0.0.1', port: 1234};
+const TEST_DATA = 'PING';
+
+
+var server = new net.Socket(net.AF_INET, net.SOCK_DGRAM);
+server.bind(TEST_ADDR);
+
+var client = new net.Socket(net.AF_INET, net.SOCK_DGRAM);
+client.bind({host: '127.0.0.1', port: 0});
+client.connect(TEST_ADDR);
+
+client.send(TEST_DATA);
+var res = server.recvfrom();
+assert.equal(res.data, TEST_DATA);
+assert.deepEqual(res.address, client.getsockname());
+
+server.close();
+client.close();
+
---
+++ test/test-net-socket-dgram.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const assert = require('assert');
+const net = require('net');
+
+
+const TEST_ADDR = {host: '127.0.0.1', port: 1234};
+const TEST_DATA = 'PING';
+
+
+var server = new net.Socket(net.AF_INET, net.SOCK_DGRAM);
+server.bind(TEST_ADDR);
+
+var client = new net.Socket(net.AF_INET, net.SOCK_DGRAM);
+client.bind({host: '127.0.0.1', port: 0});
+
+client.sendto(TEST_DATA, TEST_ADDR);
+var res = server.recvfrom();
+assert.equal(res.data, TEST_DATA);
+assert.deepEqual(res.address, client.getsockname());
+
+server.close();
+client.close();
