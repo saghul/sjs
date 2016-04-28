262d9bcc8523bb2e021679207f9c74aa6c84fca8
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-04-28 09:30:20 +0200

    test: add socket connection test

---
+++ test/test-net-socket-connection.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const assert = require('assert');
+const net = require('net');
+
+
+const TEST_ADDR = {host: '127.0.0.1', port: 1234};
+const TEST_DATA = 'PING';
+
+var server = new net.Socket(net.AF_INET, net.SOCK_STREAM);
+server.bind(TEST_ADDR);
+server.listen(128);
+
+var client = new net.Socket(net.AF_INET, net.SOCK_STREAM);
+client.connect(TEST_ADDR);
+
+var connection = server.accept();
+
+connection.send(TEST_DATA);
+var data = client.recv();
+assert.equal(data, TEST_DATA);
+client.send(TEST_DATA);
+var data = connection.recv();
+assert.equal(data, TEST_DATA);
+
+server.close();
+client.close();
+connection.close();
+
