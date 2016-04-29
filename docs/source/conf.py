b501396bd112178e67bc80377b39fb4e11bd5c0c
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-04-29 20:13:48 +0200

    doc: add manpage plugin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('sphinx-plugins'))
 
 # -- General configuration ------------------------------------------------
 
@@ -28,7 +29,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['alabaster']
+extensions = ['alabaster', 'manpage']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
