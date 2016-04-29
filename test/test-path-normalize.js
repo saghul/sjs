bf70e0cb28ee14e2d4191215b3469ae3387062a6
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-04-29 20:24:48 +0200

    test: add test case for path.normalize

---
+++ test/test-path-normalize.js
@@ -0,0 +1,10 @@
+
+const assert = require('assert');
+const path = require('path');
+
+var p = path.normalize('~');
+assert.notEqual(p, '~');
+
+var bogusPath = '/this/path/wont/exist';
+var p = path.normalize(bogusPath);
+assert.equal(p, bogusPath);
