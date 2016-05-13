56d5a1b5483effa2e8c2d85b535dc0976a2dbdb9
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-05-13 17:20:02 +0100

    fs: fix compilation warning on Linux

--- modules/fs.cmake
+++ modules/fs.cmake
@@ -9,6 +9,11 @@ set_target_properties(_fs PROPERTIES
     PREFIX ""
     SUFFIX ".jsdll"
 )
+if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+    set_target_properties(_fs PROPERTIES
+        COMPILE_DEFINITIONS "_POSIX_C_SOURCE=200112"
+    )
+endif()
 install(TARGETS _fs
         DESTINATION lib/sjs/modules)
 
