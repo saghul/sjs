66c850347187cff89977a5ff5a3fbb7e3e5a148d
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-06-13 11:39:18 +0100

    test: simplified test
    There is no need to add the current directory to the SJS_PATH because
    with the new require system inplace relative requires work properly.

--- test/test-shebang-require.js
+++ test/test-shebang-require.js
@@ -1,11 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const path = require('path');
-const system = require('system');
-
-
-system.path.unshift(path.dirname(__filename));
 const shebangtest = require('./shebangtest');
+
 
 assert.equal(shebangtest.foo(), 42);
