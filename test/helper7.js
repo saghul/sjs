23c58a373faf38a914c37dd351f049b11a712b35
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-06-14 01:39:18 +0200

    test: add test for require.main property

---
+++ test/helper7.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const assert = require('assert');
+
+assert(require.main === undefined);
---
+++ test/test-require-main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const assert = require('assert');
+
+
+assert(require.main === module);
+require('./helper7')
