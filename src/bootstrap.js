48a6547f43fb00309522ad242f8c5972ccf5b0e7
Saúl Ibarra Corretgé <s@saghul.net>
2018-05-13 09:38:06 +0200

    vm: remove globals which don't really work (yet)

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,1 +1,8 @@
-const console = require('console');
+global.console = require('console');
+
+// These are not supported yet, but babel-polyfill injects some.
+delete global.Promise;
+delete global.setTimeout;
+delete global.clearTimeout;
+delete global.setImmediate;
+delete global.clearImmediate;
