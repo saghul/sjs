2b0a8dce89998764fc08a1fde4d67a5d746c6a45
Saúl Ibarra Corretgé <s@saghul.net>
2018-05-27 23:34:20 +0200

    cli: only show detailed build info on Debug builds

--- src/cli/greet.js
+++ src/cli/greet.js
@@ -1,4 +1,6 @@
 console.log('Skookum JS ' + system.versions.sjs + ' running on ' + system.platform + ' ' + system.arch);
-console.log('[Engine: Duktape ' + system.versions.duktape + ' (' + system.versions.duktapeCommit + ')]');
-console.log('[Build: ' + system.build.type + ' on ' + system.build.timestamp + ']');
-console.log('[' + system.build.compiler + ' ' + system.build.compilerVersion + ' on ' + system.build.system + ']');
+if (system.build.type === 'Debug') {
+    console.log('[Engine: Duktape ' + system.versions.duktape + ' (' + system.versions.duktapeCommit + ')]');
+    console.log('[Build: ' + system.build.type + ' on ' + system.build.timestamp + ']');
+    console.log('[' + system.build.compiler + ' ' + system.build.compilerVersion + ' on ' + system.build.system + ']');
+}
